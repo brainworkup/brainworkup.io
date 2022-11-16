66067d99f57fc540d2379a2efc01ea39baea714a
brainworkup <j.trampush@gmail.com>
2022-11-16 12:57:33 -0800

    working ...

--- R/blogdown.R
+++ R/blogdown.R
@@ -1,23 +1,34 @@
 # blogdown
 
-# check stuff
-# these sometimes gets used after check_site()
-blogdown::hugo_build()
 blogdown::hugo_build(local = TRUE)
 
+blogdown::serve_site()
+
+install.packages(c("pak", "languageserver"))
+
+blogdown::serve_site()
+blogdown::stop_server()
+
+blogdown::hugo_build()
 blogdown::build_site()
-blogdown::build_site(build_rmd = "content/post/2021-06-18-npsych-drilldown/index.Rmd") # to render a file
+blogdown::check_hugo()
+blogdown::check_netlify()
+
+# check stuff
+blogdown::check_site()
+
+# these sometimes gets used after check_site()
+rmarkdown::clean_site(preview = FALSE) # this might delete everything in "public"
+
+rmarkdown::clean_site(preview = TRUE)
+
+blogdown::clean_duplicates(preview = FALSE)
+
 blogdown::build_site(build_rmd = "timestamp") # to update by re-rendering
+
+blogdown::build_site(build_rmd = "newfile") # to render a file
 
 blogdown::check_config()
 blogdown::check_gitignore()
 blogdown::check_hugo()
 blogdown::check_netlify()
-blogdown::check_site()
-
-blogdown::clean_duplicates(preview = FALSE)
-rmarkdown::clean_site(preview = FALSE) # this deletes everything in "public"
-rmarkdown::clean_site(preview = TRUE)
-
-blogdown::serve_site()
-blogdown::stop_server()
